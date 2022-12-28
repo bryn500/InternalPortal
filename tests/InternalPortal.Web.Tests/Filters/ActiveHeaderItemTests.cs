28f3f66a5aea6b157191ff8fc08e49b1be39c508
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-12-28 16:01:35 +0000

    Add active header item tests

---
+++ tests/InternalPortal.Web.Tests/Filters/ActiveHeaderItemTests.cs
@@ -0,0 +1,41 @@
+﻿using InternalPortal.Web.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using InternalPortal.Web.Controllers;
+
+namespace InternalPortal.Web.Tests.Filters
+{
+    [TestClass]
+    public class ActiveHeaderItemTests
+    {
+        [TestMethod]
+        public void ActiveHeaderItemFilterAttribute_SetsViewBag()
+        {
+            //arrange
+            var requiredHeader = "Test";
+            var actionFilter = new ActiveHeaderItemFilterAttribute(requiredHeader);
+            var homeController = new HomeController(null);
+            var httpContext = new DefaultHttpContext();
+
+            var actionContext = new ActionContext(httpContext,
+                new RouteData(),
+                new ActionDescriptor(),
+                new ModelStateDictionary());
+
+            var actionExecutingContext = new ActionExecutingContext(actionContext,
+                new List<IFilterMetadata>(),
+                new Dictionary<string, object?>(),
+                controller: homeController);
+
+            //act
+            actionFilter.OnActionExecuting(actionExecutingContext);
+
+            //assert
+            Assert.AreEqual(requiredHeader, homeController.ViewBag.ActiveHeaderItem);            
+        }
+    }
+}
