f99d7d05e8f9a7299aa6d3e97c14a808c4338b17
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-12-28 12:59:41 +0000

    Add apis view model tests

--- src/InternalPortal.Web/Models/Apis/ApisViewModel.cs
+++ src/InternalPortal.Web/Models/Apis/ApisViewModel.cs
@@ -6,34 +6,30 @@
         public int Skipped { get; set; }
         public int Taken { get; set; }
         public List<ApiViewModel> Apis { get; set; }
+                
 
         public bool ShowPreviousLink
-        {
-            get { return Skipped > 0; }
-        }
-        public bool ShowNextLink
-        {
-            get { return Skipped + Taken < Total; }
-        }
+        { get { return Skipped > 0; } }
         public int PreviousPage
         { get { return (int)Math.Ceiling((double)Skipped / Taken); } }
-
         public string PreviousPageLink
         { get { return $"/apis?skip={Skipped - Taken}&take={Taken}"; } }
 
-        public string NextPageLink
-        { get { return $"/apis?skip={Skipped + Taken}&take={Taken}"; } }
-
+        public bool ShowNextLink
+        { get { return Skipped + Taken < Total; } }
         public int NextPage
         { get { return PreviousPage + 2; } }
+        public string NextPageLink
+        { get { return $"/apis?skip={Skipped + Taken}&take={Taken}"; } }
+                
         public int TotalPages
         { get { return (int)Math.Ceiling((double)Total / Taken); } }
 
-        public ApisViewModel(int? total, int skipped, int taken, List<ApiViewModel>? apis)
+        public ApisViewModel(int? total, int skipped, int taken, List<ApiViewModel>? apis = null)
         {
             Total = total.HasValue ? total.Value : 0;
             Skipped = skipped;
-            Taken = taken;
+            Taken = taken < 1 ? 1 : taken;
             Apis = apis != null ? apis : new List<ApiViewModel>();
         }
     }
---
+++ tests/InternalPortal.Web.Tests/Models/Apis/ApisViewModelTests.cs
@@ -0,0 +1,119 @@
+﻿using InternalPortal.Web.Models.Apis;
+
+namespace InternalPortal.Web.Tests.Models.Apis
+{
+    [TestClass]
+    public class ApisViewModelTests
+    {
+        [TestMethod]
+        [DataRow(0, 0, 0)]
+        [DataRow(null, 0, 0)]
+        [DataRow(-1, -1, -1)]
+        public void ApisViewModel_WorksWith_NoData(int? total, int skipped, int taken)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.IsNotNull(model);
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, 0, 0)]
+        [DataRow(10, 0, 1, 10)]
+        [DataRow(9, 0, 10, 1)]
+        [DataRow(100, 0, 9, 12)]
+        public void ApisViewModel_TotalPages_RoundsUp(int total, int skipped, int taken, int expected)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.AreEqual(expected, model.TotalPages);
+        }
+
+        [TestMethod]
+        [DataRow(0, 1, 0, true)]
+        [DataRow(0, 100, 0, true)]
+        [DataRow(0, 0, 0, false)]
+        [DataRow(0, -1, 0, false)]
+        public void ApisViewModel_ShowsPreviousPageLink(int total, int skipped, int taken, bool expected)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.AreEqual(expected, model.ShowPreviousLink);
+        }
+
+        [TestMethod]
+        [DataRow(0, 11, 5, 3)]
+        [DataRow(0, 10, 5, 2)]
+        [DataRow(0, 9, 5, 2)]
+        [DataRow(0, 6, 5, 2)]
+        public void ApisViewModel_PreviousPage_RoundsUp(int total, int skipped, int taken, int expected)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.AreEqual(expected, model.PreviousPage);
+        }
+
+        [TestMethod]
+        [DataRow(0, 10, 5, "/apis?skip=5&take=5")]
+        [DataRow(0, 15, 10, "/apis?skip=5&take=10")]
+        [DataRow(0, 0, 5, "/apis?skip=-5&take=5")]
+        public void ApisViewModel_PreviousPageLink_LinksToPreviousSet(int total, int skipped, int taken, string expected)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.AreEqual(expected, model.PreviousPageLink);
+        }
+
+        [TestMethod]
+        [DataRow(1, 1, 1, false)]
+        [DataRow(2, 1, 1, false)]
+        [DataRow(3, 1, 1, true)]
+        [DataRow(0, 0, 0, false)]
+        [DataRow(0, -1, 0, false)]
+        [DataRow(0, 0, -1, false)]
+        public void ApisViewModel_ShowsNextPageLink(int total, int skipped, int taken, bool expected)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.AreEqual(expected, model.ShowNextLink);
+        }
+
+        [TestMethod]
+        [DataRow(0, 11, 5, 5)]
+        [DataRow(0, 10, 5, 4)]
+        [DataRow(0, 9, 5, 4)]
+        [DataRow(0, 6, 5, 4)]
+        public void ApisViewModel_NextPage_RoundsUp(int total, int skipped, int taken, int expected)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.AreEqual(expected, model.NextPage);
+        }
+
+        [TestMethod]
+        [DataRow(0, 10, 5, "/apis?skip=15&take=5")]
+        [DataRow(0, 15, 10, "/apis?skip=25&take=10")]
+        [DataRow(0, 0, 5, "/apis?skip=5&take=5")]
+        public void ApisViewModel_NextPageLink_LinksToNextSet(int total, int skipped, int taken, string expected)
+        {
+            // act
+            var model = new ApisViewModel(total, skipped, taken);
+
+            // assert
+            Assert.AreEqual(expected, model.NextPageLink);
+        }
+    }
+}
