10918e3b49cf10b8a12bdfc881170db40ec1662f
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2022-12-28 15:49:14 +0000

    Add validation extension tests

---
+++ tests/InternalPortal.Web.Tests/Extensions/ValidationExtensionTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using InternalPortal.Web.Extensions;
+
+namespace InternalPortal.Web.Tests.Extensions
+{
+    [TestClass]
+    public class ValidationExtensionTests
+    {
+        [TestMethod]
+        public void FieldHasError_ReturnsTrue_WithError()
+        {
+            // arrange
+            var modelState = new ModelStateDictionary();
+            modelState.AddModelError("a", "b");
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+            var viewData = new ViewDataDictionary<string>(modelMetadataProvider, modelState);
+
+            // act
+            var result = viewData.FieldHasError("a");
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void FieldHasError_ReturnsFalse_WithoutError()
+        {
+            // arrange
+            var modelState = new ModelStateDictionary();
+            var modelMetadataProvider = new EmptyModelMetadataProvider();
+            var viewData = new ViewDataDictionary<string>(modelMetadataProvider, modelState);
+
+            // act
+            var result = viewData.FieldHasError("a");
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
